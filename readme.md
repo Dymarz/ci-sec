2fd1129d07aa9fb45fa436230153cee81ad8c711
Marcel Dymarz <marcel.dymarz@lichtblick.de>
2022-12-05 23:21:04 +0100

    playground moved tro wasttesting

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Dynamic Application Security Testing [![pipeline status](https://gitlab.com/dymi/ci-sec/badges/master/pipeline.svg)](https://gitlab.com/dymi/ci-sec/-/commits/master)
--- wasttesting/zapscan/wait-for-it.sh
+++ wasttesting/zapscan/wait-for-it.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+# Use this script to test if a given TCP host/port are available
+
 WAITFORIT_cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
