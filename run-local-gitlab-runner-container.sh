9ae4303248aecae39e5fab251a1f9fec2e36a925
Marcel Dymarz <m.dymarz@hotmail.de>
2022-12-06 20:14:35 +0100

    wubwub. LOKALES PIPELINING! :D unter LINUX

--- run-local-gitlab-runner-container.sh
+++ run-local-gitlab-runner-container.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-NAME="gitlab-runner1"
-
-docker run -d \
-  --name $NAME \
-  --restart always \
-  -v /$PWD:/app \
-  -v //var/run/docker.sock:/var/run/docker.sock \
-  -v gitlab-runner-config:/etc/gitlab-runner \
-  gitlab/gitlab-runner:latest
-echo
-docker exec -it -w //app $NAME gitlab-runner exec docker wasttesting
+docker run --entrypoint bash --rm -w $PWD -v $PWD:$PWD -v /var/run/docker.sock:/var/run/docker.sock --privileged gitlab/gitlab-runner:latest -c 'git config --global --add safe.directory "*"; gitlab-runner exec  docker --docker-privileged wasttesting'
