865f54998fbbca319b1d5a7b0b63b07b8832987e
Marcel Dymarz <marcel.dymarz@lichtblick.de>
2022-12-07 00:41:02 +0100

    lokal gitlab runner tests

--- run-local-gitlab-runner.sh
+++ run-local-gitlab-runner.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
+#!/bin/sh
+
+#linux
 docker run --entrypoint bash --rm -w $PWD -v $PWD:$PWD -v /var/run/docker.sock:/var/run/docker.sock --privileged gitlab/gitlab-runner:latest -c 'git config --global --add safe.directory "*"; gitlab-runner exec  docker --docker-privileged wasttesting'
